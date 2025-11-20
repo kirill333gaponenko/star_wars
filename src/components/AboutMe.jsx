fd6d6b7273250d04cc4356ac85924b686b2a4c23
kirill <kirill56gaponenko@gmail.com>
2025-11-20 20:05:24 +0200

    Update `starWarsInfo` constant in `constants.js` to remove unnecessary footnote markers and enhance readability.

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,8 +1,26 @@
+import {useEffect, useState} from "react";
+import {base_url} from "../utils/constants.js";
+
 const AboutMe = () => {
+
+    const [aboutMe, setAboutMe] = useState();
+
+    useEffect(() => {
+
+        fetch(`${base_url}/v1/peoples/1`)
+            .then(res => res.json())
+            .then(data => {
+                let {id,edited,created,homeworld,image,...restOfData} = data,
+                setAboutMe(restOfData)
+            }
+            )
+            .catch(() => setAboutMe('Error'));
+        // return () => console.log('Component OpeningCrawl unmounted');
+    }, [])
+
     return (
-        <div>
-            About me
-        </div>
+        <p className="far-galaxy">{aboutMe}</p>
+
     )
 }
 
